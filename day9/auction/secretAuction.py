8d9682861946c6bd0cefeadaaa2b723cfa9cc926
Dazurashi <enaqueed@gmail.com>
2023-06-11 19:56:13 +0300

    day9

--- day9/auction/secretAuction.py
+++ day9/auction/secretAuction.py
@@ -1,18 +1,20 @@
+import os
 from art import logo
 print(logo)
 
 people = {}
 
-#TODO find the highest bid if there's no other people entering the auction
-#TODO clear the console if there are more people so the next one doesn't see how much the previous bid
-
 while True:
     name = input("Name: ")
-    bid = input("Bid: ")
+    bid = input("Bid: $")
     people[name] = bid
     #print(people)
-    answer = input("Are there more people? Answer yes or no: ")
+    answer = input("Are there more people? Answer yes or no: ").lower()
     if answer == "yes":
+        os.system("clear")
         continue
     else:
         break
+
+highestBid = max(people)
+print(f"Highest bid was from {highestBid} with ${people[highestBid]}")
