bd533895d834ff2d4a0f561ebe7dbf6e380a7d83
Dazurashi <enaqueed@gmail.com>
2023-06-19 21:47:56 +0300

    almost done

--- day15/virtualCoffeeMachine/main.py
+++ day15/virtualCoffeeMachine/main.py
@@ -1,18 +1,42 @@
 from stats import MENU, resources
 
-menu = MENU
-res = resources
+current_drink = {}
 
-#TODO Just the start. I know what to do, just pushing this for now
-def getReport(*args):
+#TODO give back the amount of money that was over the price of purchase / refund the money if there's not enough
+#TODO reduce the amount of incredients from the resources if everything else is fine and then print the result
+
+def getReport():
     '''Prints the remaining resources'''
-    return res
+    print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${resources['money']}")
 
-machine_functions = {"report" : getReport}
+def checkResources(ordered_drink):
+    for i in ordered_drink:
+        if ordered_drink[i] > resources[i]:
+            print(f"Sorry not enough {i}")
+            return False
+    return True
+
+def checkMoney(ordered_drink):
+    if ordered_drink > resources['money']:
+        print("Sorry not enough money inserted")
+        return False
+    else:
+        return True
+
+def reduce_resources(res, drink):
+    pass
+
 while True:
-    order = input("What would you like? (espresso/latte/cappuccino): ")
+    order = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if order == "off":
         break
-    
-    new_function = machine_functions[order]
-    print(new_function({}))    
+    elif order == "report":
+        getReport()
+    else:
+        current_drink = MENU[order]
+        if checkResources(current_drink['ingredients']):
+            inserted_money = float(input("Please insert money: $"))
+            resources["money"] =+ inserted_money
+            if checkMoney(current_drink['cost']):
+                taken_resources = reduce_resources(resources, current_drink['ingredients'])
+                
