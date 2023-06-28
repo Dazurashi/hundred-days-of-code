e101b796cba3349efe28dae2cb88dd14f53542cc
Dazurashi <enaqueed@gmail.com>
2023-06-28 23:39:44 +0300

    hiss2

--- day20/snake-game-part1/main.py
+++ day20/snake-game-part1/main.py
@@ -1,13 +1,30 @@
 import turtle as t
+import time
 
 screen = t.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('orange')
 screen.title("Snake Game")
+screen.tracer(0)
+game_running = True
+
+segments = []
 
 starting_pos = [(0, 0), (-20, 0), (-40, 0)]
 for pos in starting_pos:
     block = t.Turtle('square')
+    block.penup()
     block.goto(pos)
+    segments.append(block)
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+    for i in segments:
+        i.forward(20)
+        
+
+
+
 
 screen.exitonclick()
