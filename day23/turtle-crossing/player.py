bc676ecdd161c3893cf634a72141b3dbb8c1e3c7
Dazurashi <enaqueed@gmail.com>
2023-07-12 15:47:28 +0300

    turtle moves

--- day23/turtle-crossing/player.py
+++ day23/turtle-crossing/player.py
@@ -1,7 +1,18 @@
+import turtle as t
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(t.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.left(90)
+
+    def move (self):
+        y_cord = self.ycor() + MOVE_DISTANCE
+        self.goto(0, y_cord)
