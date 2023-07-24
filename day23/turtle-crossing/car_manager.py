5a4abbb5ce0f33d736da99e2297c59ad622c0d41
Dazurashi <enaqueed@gmail.com>
2023-07-24 18:59:34 +0300

    cars move

--- day23/turtle-crossing/car_manager.py
+++ day23/turtle-crossing/car_manager.py
@@ -1,4 +1,4 @@
-import turtle as t
+from turtle import Turtle
 import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
@@ -6,11 +6,21 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(t.Turtle):
+class CarManager():
     def __init__(self):
-        super().__init__()
-        self.shape('square')
-        self.color(random.choice(COLORS))
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        self.penup()
-        self.goto(0, 0)
+        self.all_cars = []
+
+    def create_car(self):
+        car_frequency = random.randint(1, 6)
+        if car_frequency == 1: 
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid= 1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y_pos = random.randint(-250, 250)
+            new_car.goto(300, random_y_pos)
+            self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- day23/turtle-crossing/main.py
+++ day23/turtle-crossing/main.py
@@ -18,3 +18,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car.create_car()
+    car.move_car()
