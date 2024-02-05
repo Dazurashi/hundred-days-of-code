3040faf9ff52ce1757ccb91a842493fe7b681622
Dazurashi <enaqueed@gmail.com>
2024-02-05 14:03:52 +0200

    small adjustment

--- day27/main.py
+++ day27/main.py
@@ -14,14 +14,14 @@ def miles_to_km():
 
 miles_entry = tk.Entry()
 text_label = tk.Label(text=" miles is ")
-km_label = tk.Label(text="_____")
+km_label = tk.Label(text="0")
 text_label2 = tk.Label(text=" kilometers")
 transform_button = tk.Button(text="convert", command=miles_to_km)
 
 miles_entry.grid(column=0, row=0)
 text_label.grid(column=1, row=0)
-km_label.grid(column=2, row=0)
-text_label2.grid(column=3, row=0)
-transform_button.grid(column=0, row=1)
+km_label.grid(column=0, row=1)
+text_label2.grid(column=1, row=1)
+transform_button.grid(column=0, row=2)
 
 window.mainloop()
